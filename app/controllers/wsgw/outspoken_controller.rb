7e4f8ba2c39567615acc5fbe44895f184c8ce1da
Ryan Her <ryan@quikly.com>
2014-07-22 11:34:17 -0400

    Constant delay of 3 seconds for each send_job for testing

--- app/controllers/wsgw/outspoken_controller.rb
+++ app/controllers/wsgw/outspoken_controller.rb
@@ -10,13 +10,11 @@ class Wsgw::OutspokenController < ApplicationController
       @job_request_id = job_xml.root.elements["job-request-id"].try(:text) or raise Outspoken::MissingJobRequestId
       @num_destinations = (job_xml.root.elements["recipient-count"].try(:text) || 1).to_i
       # Block process for given delay or for random seconds between 0.5 and 2.5
-      response_delay = params[:delay] =~ /^\d{1,2}$/ ? params[:delay].to_i : (rand(20) + 5).to_f / 10
+      response_delay = params[:delay] =~ /^\d{1,2}$/ ? params[:delay].to_i : 3 #(rand(20) + 5).to_f / 10
       sleep(response_delay) if response_delay > 0
     end
 
     status_code = (params[:random_failure] && rand(params[:random_failure].to_i) < 1) ? [2,3,5,6].sample : 0
-
-    puts "Processing Job #{@job_request_id}" if @job_request_id
 
     if @sms_job_log = SmsJobLog.where(job_request_id: @job_request_id).order('created_at DESC').first
       # If job already exists, return this error code
@@ -24,6 +22,8 @@ class Wsgw::OutspokenController < ApplicationController
     else
       @sms_job_log = SmsJobLog.create(job_request_id: @job_request_id, duration: t, status_code: status_code, num_destinations: @num_destinations, job_xml: job_xml.to_s)
     end
+
+    puts "Processing Job #{@job_request_id}. Status: #{@sms_job_log.status_code}" if @job_request_id
 
     render :template => 'wsgw/outspoken/job_status.txt.erb', :layout => false
   end
